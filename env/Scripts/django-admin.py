3dddfe830acc991141b9e592aa66f5921b1092ee
Abbosjon <squarefozilov@gmail.com>
2020-03-04 20:31:35 -0500

    *

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Abbosjon\Desktop\ujifjiwef\my-first-blog\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
